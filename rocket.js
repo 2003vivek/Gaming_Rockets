1033ff25bb8fb7ce54405fcb3f8b5c17aa3d6840
Harsh <145972737+Harsh-Codes-382@users.noreply.github.com>
2023-11-08 12:51:33 +0530

    Added the rocket animation

--- rocket.js
+++ rocket.js
@@ -10,69 +10,77 @@ const rocket = document.getElementById("rocket");
 const earnedAmountDisplay = document.getElementById("earned-amount");
 
 decreaseButton.addEventListener("click", () => {
-    if (currentAmount >= 50) {
-        currentAmount -= 50;
-        updateAmountDisplay();
-    }
+  if (currentAmount >= 50) {
+    currentAmount -= 50;
+    updateAmountDisplay();
+  }
 });
 
 increaseButton.addEventListener("click", () => {
-    if (currentAmount < 10000) {
-        currentAmount += 50;
-        updateAmountDisplay();
-    }
+  if (currentAmount < 10000) {
+    currentAmount += 50;
+    updateAmountDisplay();
+  }
 });
 
 startButton.addEventListener("click", () => {
-    if (!rocketLaunched) {
-        startRocketMove();
-        rocketLaunched = true;
-        increaseAmountContinuously();
-    }
+  let am = 10;
+  if (!rocketLaunched) {
+
+    setInterval(() => {
+      am += 10;
+      startRocketMove(am);
+    }, 100)
+
+    rocketLaunched = true;
+    increaseAmountContinuously();
+  }
 });
 
 function updateAmountDisplay() {
-    currentAmountDisplay.innerText = currentAmount;
+  startRocketMove(currentAmount);
+  currentAmountDisplay.innerText = currentAmount;
 }
 
-function startRocketMove() {
-    rocket.style.left = "0";
-    rocket.style.bottom = "0";
-    rocket.style.display = "block";
-    rocket.style.transition = "transform 2s cubic-bezier(0.17, 0.67, 0.83, 0.67)";
-    rocket.style.transform = "translateX(100%) translateY(-50%) rotate(-90deg)";
-    
-    setTimeout(() => {
-        rocket.style.display = "none";
-        rocketLaunched = false;
-        displayRandomBlast();
-    }, 2000);
+function startRocketMove(amount) {
+  rocket.style.left = "0";
+  rocket.style.bottom = "0";
+  rocket.style.display = "display"
+  rocket.style.transition = "transform 1s cubic-bezier(0.17, 0.67, 0.83, 0.67)";
+  rocket.style.transform = `translateX(${(amount % 1000) + 100}%)
+  translateY(-${(amount % 1000) - 20}%) rotate(5deg)`;
+
+  setTimeout(() => {
+    rocket.style.display = "none";
+    rocketLaunched = false;
+    displayRandomBlast();
+  }, 6000);
 }
 
 function increaseAmountContinuously() {
-    const interval = 1000;
-    const increaseRate = 0.5;
-    const maxAmount = 10000;
+  const interval = 1000;
+  const increaseRate = 0.5;
+  const maxAmount = 10000;
 
-    const intervalId = setInterval(() => {
-        if (rocketLaunched) {
-            earnedAmount += increaseRate;
-            if (earnedAmount >= maxAmount) {
-                earnedAmount = maxAmount;
-                updateAmountDisplay();
-                stopRocketMove(intervalId);
-            }
-            earnedAmountDisplay.innerText = earnedAmount;
-        }
-    }, interval);
+  const intervalId = setInterval(() => {
+    if (rocketLaunched) {
+      earnedAmount += increaseRate;
+      if (earnedAmount >= maxAmount) {
+        earnedAmount = maxAmount;
+        updateAmountDisplay();
+        stopRocketMove(intervalId);
+      }
+
+      earnedAmountDisplay.innerText = earnedAmount;
+    }
+  }, interval);
 }
 
 function stopRocketMove(intervalId) {
-    clearInterval(intervalId);
+  clearInterval(intervalId);
 }
 
 function displayRandomBlast() {
-    // Implement the rocket blast logic, choose a random position
-    // and add the earned amount to the player's total amount.
+  // Implement the rocket blast logic, choose a random position
+  // and add the earned amount to the player's total amount.
 }
-
